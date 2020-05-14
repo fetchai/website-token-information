7b684cf069a383e8d02f5423a9ce514c1d8af8ac
Bart Centlewski <bart@fetch.ai>
2020-05-14 16:33:19 +0100

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i "s/PGUSER=test/PGUSER=${PGUSER}/" package.json
-sed -i "s/PGHOST=localhost/PGHOST=${PGHOST=}}/" package.json
+sed -i "s/PGHOST=localhost/PGHOST=${PGHOST=}/" package.json
 sed -i "s/PGPASSWORD=myPassword/PGPASSWORD=${PGPASSWORD}/" package.json
 sed -i "s/PGDATABASE=douglas/PGDATABASE=${PGDATABASE}/" package.json
 
