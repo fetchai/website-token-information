6a006028c2b84f765b5378c664fe1c495126cb0a
dougb99 <douglaswburkinshaw@hotmail.co.uk>
2020-11-03 16:12:45 +0000

    w

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ sed -i "s/\/\/ process.env.PGHOST = PGHOST/process.env.PGHOST = \"${PGHOST}\"/" 
 sed -i "s/\/\/ process.env.PGPASSWORD = PGPASSWORD/process.env.PGPASSWORD = \"${PGPASSWORD}\"/" server/index.js
 sed -i "s/\/\/ process.env.PGDATABASE = PGDATABASE/process.env.PGDATABASE = \"${PGDATABASE}\"/" server/index.js
 sed -i "s/\/\/ process.env.runScraper = runScraper/process.env.runScraper = \"false\"" server/index.js
-sed -i "s/\/\/ process.env.PGPORT = \"5432\"/process.env.PGPORT = \"5432\"/" server/index.js
 sed -i "s/const ETHERSCAN_API_KEY = ETHERSCAN_API_KEY/const ETHERSCAN_API_KEY = \"${ETHERSCAN_API_KEY}\"/" server/index.js
 
 npm run prod
